a22fb88100bdaf87c8edd34bd7700528d7eb349a
Weikeng Chen <w.k@berkeley.edu>
2022-12-17 15:21:19 -0800

    Remove the Canaan curve for now (#9)
    Co-authored-by: onewayfunc <onewayfunc@gmail.com>

--- src/curve/canaan/fq.rs
+++
@@ -1,7 +0,0 @@
-use ark_ff::fields::{Fp320, MontBackend, MontConfig};
-
-#[derive(MontConfig)]
-#[modulus = "115792089237316195423570985008687907853634386693684621307141857813043191627553"]
-#[generator = "5"]
-pub struct FqConfig;
-pub type Fq = Fp320<MontBackend<FqConfig, 5>>;
--- src/curve/canaan/fr.rs
+++
@@ -1,2 +0,0 @@
-pub use ark_secp256k1::Fq as Fr;
-pub use ark_secp256k1::FqConfig as FrConfig;
